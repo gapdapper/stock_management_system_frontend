c40ddb7af3c5a1d321b894750c2766cedc8c71c3
Gap <puri.pongsomboonsuk@gmail.com>
2026-01-14 22:14:00 +0700

    feat: add prop to barChartSection

--- src/features/dashboard/components/barChartSection.tsx
+++ src/features/dashboard/components/barChartSection.tsx
@@ -1,59 +1,44 @@
-import { Bar, BarChart, CartesianGrid, LabelList, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import type { IChartData } from "@/app/types/dashboard";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  LabelList,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-export default function BarChartSection() {
-  const data = [
-    {
-      name: "Chess",
-      sold: 531,
-    },
-    {
-      name: "Jenga",
-      sold: 499,
-    },
-    {
-      name: "Domino",
-      sold: 452,
-    },
-    {
-      name: "Jackpot",
-      sold: 421,
-    },
-    {
-      name: "Dice Roller",
-      sold: 377,
-    },
-  ];
+type Prop = {
+  data: IChartData[];
+};
+
+export default function BarChartSection({ data }: Prop) {
   return (
     <>
-    <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data} layout="vertical">
-        <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} layout="vertical">
+          <CartesianGrid strokeDasharray="3 3" horizontal={false} />
 
-        <XAxis
-          type="number"
-          label={{
-            value: "Sold Amount",
-            position: "insideBottom",
-            offset: -5,
-          }}
-        />
-
-        <YAxis
-          type="category"
-          dataKey="name"
-          width={110}
-        />
-
-        <Tooltip />
-
-        <Bar dataKey="sold" fill="#82ca9d" radius={[0, 6, 6, 0]}>
-          <LabelList
-            dataKey="sold"
-            position="right"
+          <XAxis
+            type="number"
+            label={{
+              value: "Sold Amount",
+              position: "insideBottom",
+              offset: -5,
+            }}
           />
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+
+          <YAxis type="category" dataKey="name" width={110} />
+
+          <Tooltip />
+
+          <Bar dataKey="value" fill="#82ca9d" radius={[0, 6, 6, 0]}>
+            <LabelList dataKey="value" position="right" />
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
     </>
   );
 }
--- src/utils/dashboard.ts
+++ src/utils/dashboard.ts
@@ -1,4 +1,4 @@
-type DonutData = {
+type IChartData = {
   name: string;
   value: number;
 };
@@ -7,7 +7,7 @@ export const normalizeDonutData = <T,>(
   data: T[],
   labelKey: keyof T,
   valueKey: keyof T
-): DonutData[] => {
+): IChartData[] => {
   return data.map(item => ({
     name: String(item[labelKey]),
     value: Number(item[valueKey]),

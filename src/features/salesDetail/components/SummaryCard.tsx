2bc9468ada4bd02efb946ccf1722c5a403041120
Gap <puri.pongsomboonsuk@gmail.com>
2026-03-18 19:37:49 +0700

    feat: adjust SalesTracker header + add breadcrumb link to SalesDetail

--- src/features/salesDetail/components/SummaryCard.tsx
+++ src/features/salesDetail/components/SummaryCard.tsx
@@ -1,7 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import "./SummaryCard.scss";
-import { useNavigate } from "react-router";
 import type { ITransactions } from "@/types/transaction";
 
 type Prop = {
@@ -9,18 +6,12 @@ type Prop = {
 };
 
 export default function SummaryCard({ data }: Prop) {
-  let navigate = useNavigate();
 
   return (
     <section className="summary-card">
       {/* Header */}
       <div className="summary-header">
         <h2>
-          <FontAwesomeIcon
-            className="prev-btn"
-            icon={faCircleLeft}
-            onClick={() => navigate(`/sales`)}
-          />{" "}
           {data.orderId}
         </h2>
         <span className={`status-badge ${data.status}`}>{data.status}</span>
@@ -45,7 +36,7 @@ export default function SummaryCard({ data }: Prop) {
 
         <div className="summary-item">
           <span className="label">Created At</span>
-          <span className="value">{data.createdAt.toLocaleString()}</span>
+          <span className="value">{new Date(data.createdAt).toLocaleString()}</span>
         </div>
       </div>
 
